bf7324f0159885bc2d35ad0029c83d28fcff062e
Stephan <sschweitzer2@myges.fr>
2024-05-25 14:36:59 +0200

    updated sbt so it works now

--- src/main/scala/Producer.scala
+++ src/main/scala/Producer.scala
@@ -1,10 +1,21 @@
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
 
 object Producer
 {
   def main(args: Array[String]): Unit = {
+
+    val spark = SparkSession.builder
+      .appName("YourApp")
+      .master("local[*]") // Use local mode for running Spark locally
+      .getOrCreate()
+
+    val sc = spark.sparkContext
+
+    import spark.implicits
+
     println("Hello world ")
 
-    val spark = SparkSession.
   }
+
 }
